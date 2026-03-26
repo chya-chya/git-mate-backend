3bdab4ce3c69ab0295706f36a3be62724597c069
sooyen <cktndus32@naver.com>
2026-03-26 17:25:23 +0900

    initial commit: Git-Mate NestJS backend scaffold

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AnalysisModule } from './analysis/analysis.module';
+import { CollectionModule } from './collection/collection.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [AuthModule, AnalysisModule, CollectionModule, PrismaModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Global Validation Pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Swagger Setup
+  const config = new DocumentBuilder()
+    .setTitle('Git-Mate API')
+    .setDescription('Git-Mate Backend Service API Description')
+    .setVersion('1.0')
+    .addTag('git-mate')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
